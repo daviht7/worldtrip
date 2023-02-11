bfcd58b76b1a8589afb64767749834cdd631da40
Davi Holanda <daviht7@gmail.com>
2023-02-11 18:27:42 -0300

    projeto finalizado

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '../styles/theme'
+import NextNProgress from 'nextjs-progressbar';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Import Swiper styles
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
+
+import '../styles/slider.scss';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <NextNProgress
+        color="#FFBA08"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={5}
+      />
+        <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
+
+export default App
